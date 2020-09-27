1fe74eacddf009acbb6b84e4dbedd500bf531ac6
browny-2019 <67189412+browny-2019@users.noreply.github.com>
2020-09-27 12:02:22 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var database,gameState,playerCount,game,form,player;
+
+function setup(){
+    database = firebase.database();
+    
+    createCanvas(500,500);
+   
+}
+
+function draw(){
+    background("white");
+   
+}
+
